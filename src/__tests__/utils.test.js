4f5790ee28104298f9e107d338a9606aaf0f7e20
JakobJohnson <madjzj@gmail.com>
2024-01-24 15:00:51 -0800

    Completed Lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,31 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", ()=>{
+    it("detects if an inputted word spelt the same front to back",()=>{
+        const word = "racecar"
+        expect(isPalindrome(word)).toBe(true)
+    })
+})
+
+describe("isPalindrome", ()=>{
+    it("returns false if the word is not a palindrome",()=>{
+        const word = "car"
+        expect(isPalindrome(word)).toBe(false)
+    })
+})
+
+describe("isPalindrome", ()=>{
+    it("returns true reguardless of case",()=>{
+        const word = "RaceCar"
+        expect(isPalindrome(word)).toBe(true)
+    })
+})
+
+describe("isPalindrome", ()=>{
+    it("returns false for an empty string",()=>{
+        const word = ""
+        expect(isPalindrome(word)).toBe(false)
+    })
+})
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+export function isPalindrome(word){
+    let wo = ''
+    let rd = ''
+    for(let i = 0; i < word.length/2 ; i++ ){
+        wo += word[i]
+        rd += word[word.length-(i+1)]
+    }
+    if(wo){
+        return (wo.toLocaleLowerCase() === rd.toLocaleLowerCase()) ? true : false; 
+    }
+    return false
+    
+}
